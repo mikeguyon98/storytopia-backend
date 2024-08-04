338a9be2486746bcf6d23281144709aabcdb44fb
Michael Guyon <mikeguyon98@gmail.com>
2024-08-04 00:30:41 -0400

    Fixes pylint issues

--- storytopia_backend/models.py
+++ storytopia_backend/models.py
@@ -1,5 +1,18 @@
+"""
+This module defines the data models used in the application.
+
+Classes:
+    User: Represents a user with a username and creation date.
+"""
 from pydantic import BaseModel
 
 class User(BaseModel):
+    """
+    Represents a user with a username and creation date.
+
+    Attributes:
+        username (str): The username of the user.
+        createdAt (str): The date and time when the user was created.
+    """
     username: str
     createdAt: str
